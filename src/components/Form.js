315012bf5bcee91ef1962c5ac5ebdd3f961400b2
ugo_pc <pulito.ugo@gmail.com>
2023-05-10 13:13:49 +0200

    debut dev signin logic

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+
+    const getUsername = (e) => {
+        setUsername(e.target.value);
+    }
+    const getPassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const getRemember = (e) => {
+        setRemember(!remember);
+    }
+
+    const signIn = (event) => {
+        event.preventDefault();
+        ((username !== '' && password !== '') ? console.log('Username : ' + username + ' | Password : ' + password + ' | Remember me : ' + remember) : console.error('utilisateur ou mot de passe incorrect !'))
+        
+    }
+
     return (
         <form>
             <div className='input-wrapper'>
                 <label htmlFor='username'>Username</label>
-                <input type='email' id='username'/>
+                <input onChange={getUsername} type='email' id='username' required/>
             </div>
             <div className='input-wrapper'>
                 <label htmlFor='password'>Password</label>
-                <input type='password' id='password'/>
+                <input onChange={getPassword} type='password' id='password' required/>
             </div>
             <div className='input-remember'>
-                <input type='checkbox' id='remember-me'/>
-                <label htmlFor='remember-me'>Remeber me</label>
+                <input onClick={getRemember} type='checkbox' id='remember-me'/>
+                <label htmlFor='remember-me'>Remember me</label>
             </div>
-            <button className='sign-in-button'>Sign In</button>
+            <button onClick={signIn} className='sign-in-button'>Sign In</button>
         </form>
     );
 };
