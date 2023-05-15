2f82ebea82982c5d7958ea359bd1d3998d401409
Ugo <pulito.ugo@gmail.com>
2023-05-15 23:03:53 +0200

    ajout logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import './_Components.css';
-import { Link } from 'react-router-dom';
+import { authServices } from '../_utils/AuthServices';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+    const location = useLocation();
+    const logoutUser = () => {
+        authServices.logout();
+        console.log('logout')
+    }
+    const displayLogout = () => {
+        if(location.pathname === '/user'){
+            return (
+                <button onClick={logoutUser} className='nav-item'>
+                    <span className='nav-item-icon material-symbols-outlined'>logout</span>
+                    <span className="nav-item-text">Sign Out</span>
+                </button>
+            )
+        }
+    }
 
     return (
         <header>
             <Link to='/'>
                 <img className='logo' src="./img/argentBankLogo.png" alt="Logo Argent Bank"/>
             </Link>
-            <Link to='/sign-in' className='nav-item'>
-                <span className="nav-item-icon material-symbols-outlined">account_circle</span>
-                <span className='nav-item-text'>Sign In</span>
-            </Link>
+            <div className='nav-items'>
+                <Link to='/sign-in' className='nav-item'>
+                    <span className="nav-item-icon material-symbols-outlined">account_circle</span>
+                    <span className='nav-item-text'>Sign In</span>
+                </Link>
+                {displayLogout()}
+            </div>
         </header>
     );
 };
