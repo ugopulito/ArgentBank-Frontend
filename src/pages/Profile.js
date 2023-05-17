6a87ec5a50b40192af3ae72f65d3d86572c3a075
ugo_pc <pulito.ugo@gmail.com>
2023-05-17 11:56:18 +0200

    ajout modale edit username

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Transaction from '../components/Transaction';
-import { useDispatch, useSelector } from 'react-redux';
-import { saveUserData } from '../redux';
+import { useSelector } from 'react-redux';
+import Modale from '../components/Modale';
 
 const Profile = () => {
     const userStore = useSelector(state => state.User);
-    const dispatch = useDispatch();
-    const [newuserName, setnewuserName] = useState(userStore.userName)
-    const editUsername = (e) => {
-        e.preventDefault();
-        dispatch(saveUserData({name: 'userName', value: newuserName}))
-    };
     const initModale = () => {
         console.log('init Modale');
-    }
-    const cancel = (e) => {
-        e.preventDefault();
-        setnewuserName('');
     }
     return (
         <main className='profile main bg-dark'>
@@ -26,16 +16,7 @@ const Profile = () => {
                 {userStore.userName}
             </h1>
             <button onClick={initModale} className='edit-button'>Edit Name</button>
-            <form>
-                <label htmlFor='username'>User name:</label>
-                <input type='text' name='username' placeholder={userStore.userName} value={newuserName} onChange={(e) => {setnewuserName(e.target.value)}}></input>
-                <label htmlFor='firstname'>User name:</label>
-                <input type='text' name='firstname' value={userStore.firstName} disabled></input>
-                <label htmlFor='last name'>User name:</label>
-                <input type='text' name='last name' value={userStore.lastName} disabled></input>
-                <button onClick={editUsername}>Save</button>
-                <button onClick={cancel}>Cancel</button>
-            </form>
+            <Modale/>
             <Transaction 
                 title='Argent Bank Checking (x8349)' amount='$2,082.79' description='Available Balance'
             />
