a8be4e578877470adbf975056123925c00b71423
ugopulito <pulito.ugo@gmail.com>
2023-06-03 10:52:23 +0200

    ajout message erreur formulaire de connexion

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authServices } from '../_utils/AuthServices';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCredentials } from '../redux';
+import { useState } from 'react';
 
 const Form = () => {
     const authStore = useSelector(state => state.Auth);
     const dispatch = useDispatch();
-    const [remember, setRemember] = useState(false);
     const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('');
     const onChange = (e) => {
-        dispatch(setCredentials({name: e.target.name, value: e.target.value}))
+        dispatch(setCredentials({name: e.target.name, value: e.target.value}));
+        setErrorMessage('')
     }
-    const getRemember = () => {
-        setRemember(!remember);
-    }
+
     
     const login = (e) => {
         e.preventDefault();
@@ -26,29 +25,32 @@ const Form = () => {
             navigate('/user');
           })
           .catch((error) => {
-            console.log(error.response.data);
+            (error.response.data.status < 500 ? setErrorMessage('Wrong username or password.') : setErrorMessage('Network error. Please try again later'))
           })
 
-         : console.error('l\'utilisateur et/ou le mot de passe ne peuvent être vides !'))
+         : setErrorMessage('l\'utilisateur et/ou le mot de passe ne peuvent être vides !'))
         
     }
 
     return (
-        <form onSubmit={login} autoComplete='off'>
+        <>
+        <form onSubmit={login}>
             <div className='input-wrapper'>
                 <label htmlFor='email'>Username</label>
-                <input onChange={onChange} type='email' name='email' value={authStore.email} required/>
+                <input onChange={onChange} type='email' id='email' name='email' value={authStore.email} autoComplete='off' required/>
             </div>
             <div className='input-wrapper'>
                 <label htmlFor='password'>Password</label>
-                <input onChange={onChange} type='password' name='password' value={authStore.password} required/>
+                <input onChange={onChange} type='password' id='password' name='password' value={authStore.password} autoComplete='off' required/>
             </div>
             <div className='input-remember'>
-                <input onClick={getRemember} type='checkbox' id='remember-me'/>
+                <input type='checkbox' id='remember-me'/>
                 <label htmlFor='remember-me'>Remember me</label>
             </div>
             <button className='sign-in-button'>Sign In</button>
         </form>
+        {errorMessage && <p className='error'>{errorMessage}</p>}
+        </>
     );
 };
 
